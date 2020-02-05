d6aa8877773dfdb5a134ec7a2e60897c58531cd3
Thomas Ibel <tibel@users.noreply.github.com>
2020-02-05 17:00:50 +0100

    simple countdown event without reset

---
+++ src/Caerllion.Light/AsyncCountdownEvent.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Caerllion.Light
+{
+    public sealed class AsyncCountdownEvent
+    {
+        private readonly TaskCompletionSource<object> _tcs;
+        private int _remainingCount;
+
+        public AsyncCountdownEvent(int initialCount)
+        {
+            if (initialCount < 0)
+                throw new ArgumentOutOfRangeException(nameof(initialCount));
+
+            _tcs = new TaskCompletionSource<object>();
+            _remainingCount = initialCount;
+
+            if (initialCount == 0)
+                _tcs.TrySetResult(null);
+        }
+
+        public Task WaitAsync()
+        {
+            return _tcs.Task;
+        }
+
+        public void Increment()
+        {
+            var newCount = Interlocked.Increment(ref _remainingCount);
+            if (newCount == 0)
+                _tcs.TrySetResult(null);
+            else if (newCount < 0 || _tcs.Task.IsCompleted)
+                throw new InvalidOperationException();
+        }
+
+        public void Decrement()
+        {
+            var newCount = Interlocked.Decrement(ref _remainingCount);
+            if (newCount == 0)
+                _tcs.TrySetResult(null);
+            else if (newCount < 0)
+                throw new InvalidOperationException();
+        }
+    }
+}
